3ebeffbd6deed409d5e54a07e4f8287c93a730a9
PREDATOR-NIK\nikhi <nikhilpops@gmail.com>
2021-12-27 14:06:41 +0530

    flutter

---
+++ lib/view/homepage.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('This is homepage'),
+    );
+  }
+}
