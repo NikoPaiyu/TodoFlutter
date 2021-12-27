194bbb59cda45199f14f8ca2149e4b6993f248fe
PREDATOR-NIK\nikhi <nikhilpops@gmail.com>
2021-12-27 14:06:24 +0530

    Fluttter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weather/view/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
+      theme: ,
+    );
+  }
+}
